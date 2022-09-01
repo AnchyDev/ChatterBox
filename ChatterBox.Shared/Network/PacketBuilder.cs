d8e8242b3b3132f3341db16aa7c7efdcb762a1bc
AnchyDev <lewienato@gmail.com>
2022-09-01 12:18:41 +1000

    Implemented basic packet builder.

---
+++ ChatterBox.Shared/Network/PacketBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatterBox.Shared.Network
+{
+    public class PacketBuilder
+    {
+        private MemoryStream _ms;
+        private BinaryWriter _writer;
+
+        public PacketBuilder(PacketTypes packetType)
+        {
+            _ms = new MemoryStream();
+            _writer = new BinaryWriter(_ms);
+
+            byte[] packetTypeBytes = BitConverter.GetBytes((int)packetType);
+            _writer.Write(packetTypeBytes);
+        }
+
+        public PacketBuilder AppendInt(int value)
+        {
+            byte[] valueBytes = BitConverter.GetBytes(value);
+            _writer.Write(valueBytes);
+
+            return this;
+        }
+        public PacketBuilder AppendString(string value)
+        {
+            byte[] valueBytes = Encoding.UTF8.GetBytes(value);
+            _writer.Write(valueBytes);
+
+            return this;
+        }
+
+        public byte[] Build()
+        {
+            return _ms.ToArray();
+        }
+    }
+}
