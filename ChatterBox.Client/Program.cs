61ea680a2d98e9c18d71282b5f3d16a8df5b6630
AnchyDev <lewienato@gmail.com>
2022-09-12 20:37:13 +1000

    Added null/empty check for username.

--- ChatterBox.Client/Program.cs
+++ ChatterBox.Client/Program.cs
@@ -8,8 +8,15 @@ namespace ChatterBox.Client
     {
         static async Task Main(string[] args)
         {
-            Console.Write("Enter Username: ");
-            var username = Console.ReadLine();
+            string? username = string.Empty;
+
+            do
+            {
+                Console.Write("Enter Username: ");
+                username = Console.ReadLine();
+            }
+            while (string.IsNullOrEmpty(username));
+
             //var client = new ChatterClient(Dns.GetHostEntry("localhost").AddressList[0], 4411);
             var client = new ChatterClient(IPAddress.Parse("127.0.0.1"), 4411);
             client.SetUsername(username);
