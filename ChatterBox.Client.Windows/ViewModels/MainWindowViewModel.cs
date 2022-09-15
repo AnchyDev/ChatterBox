82a21e3da3da0ab7b58267f0755e50f4482498f8
AnchyDev <lewienato@gmail.com>
2022-09-15 10:50:23 +1000

    Setup project with basic MVVM principles.

---
+++ ChatterBox.Client.Windows/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatterBox.Client.Windows.ViewModels
+{
+    public class MainWindowViewModel : ViewModel
+    {
+        private string _textHelloWorld;
+        public string TextHelloWorld { get => _textHelloWorld; set => SetAndNotify(ref _textHelloWorld, value); }
+
+        public MainWindowViewModel()
+        {
+            TextHelloWorld = "Hello World!";
+        }
+    }
+}
